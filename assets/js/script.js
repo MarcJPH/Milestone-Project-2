56f7b133bbd84ce5fd80cb133eba2b0f68d3792a
MarcJPH <marchuntzinger@hotmail.com>
2021-05-05 20:47:51 +0000

    Email JS function added to script.js to email comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,26 @@ const [
     question: 'What is Spanish for bicycle'
 },
 ]
+
+
+
+
+
+//To send comments and feedback from the form in the Contact Us link
+//to email using EmailJS.
+function sendMail(contactForm) {
+    emailjs.send("service_f8z2kxb","template_06puzpp", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "comments": contactForm.comments.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false; // To block from loading a new page
+}
