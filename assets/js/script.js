723570952292e731d21f5dd49d473b83afbfa72b
MarcJPH <marchuntzinger@hotmail.com>
2021-05-11 20:06:01 +0000

    Added remaining questions to script.js file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,26 +43,138 @@ const questions = [
         {
             option2: 'Hola',
         },
-        {   
+        {
             option3: 'Bueno'
         }
-    ],        
+        ],
         answer: "option2"
     },
 
     {
         game_2: 'Question 2',
-        question: 'What is Spanish for bicycle',
-        image: 'tbc.jpg',
+        question: 'What does bicicleta mean in English?',
+        image: 'assets/images/bicycle.jpg',
         options: {
-            option1: 'Bike',
-            
-            option2: 'Scooter',
-            
-            option3: 'Tricycle'
+            option1: 'Bicycle',
+
+            option2: 'Bucket',
+
+            option3: 'Balloon'
         },
         answer: "option1"
     },
+
+    {
+        game_3: 'Question 3',
+        question: 'How do you say guitar in Spanish?',
+        image: 'assets/images/guitar.jpg',
+        options: {
+            option1: 'Getafe',
+
+            option2: 'Gorila',
+
+            option3: 'Guitarra'
+        },
+        answer: "option3"
+    },
+
+   {
+        game_3: 'Question 4',
+        question: 'What does perro mean in English?',
+        image: 'assets/images/dog.jpg',
+        options: {
+            option1: 'Dog',
+
+            option2: 'Cat',
+
+            option3: 'Parrot'
+        },
+        answer: "option1"
+    },
+    
+    {
+        game_3: 'Question 5',
+        question: 'What does playa mean in English?',
+        image: 'assets/images/beach.jpg',
+        options: {
+            option1: 'Plant',
+
+            option2: 'Party',
+
+            option3: 'Beach'
+        },
+        answer: "option3"
+    },
+
+    {
+        game_3: 'Question 6',
+        question: 'What is the colour red in Spanish?',
+        image: 'tbc.jpg',
+        options: {
+            option1: 'Blanco',
+
+            option2: 'Rojo',
+
+            option3: 'Lluvia'
+        },
+        answer: "option2"
+    },
+
+    {
+        game_3: 'Question 7',
+        question: 'What is sun in Spanish?',
+        image: 'tbc.jpg',
+        options: {
+            option1: 'Sol',
+
+            option2: 'Sello',
+
+            option3: 'Bote'
+        },
+        answer: "option1"
+    },
+
+    {
+        game_3: 'Question 8',
+        question: 'What does flor mean in English?',
+        image: 'tbc.jpg',
+        options: {
+            option1: 'Flower',
+
+            option2: 'Car',
+
+            option3: 'Foot'
+        },
+        answer: "option1"
+    },
+
+    {
+        game_3: 'Question 9',
+        question: 'What is hat in Spanish?',
+        image: 'tbc.jpg',
+        options: {
+            option1: 'Sandalias',
+
+            option2: 'Calcetines',
+
+            option3: 'Sombrero'
+        },
+        answer: "option3"
+    },
+
+    {
+        game_3: 'Question 10',
+        question: 'What does amigo mean in English?',
+        image: 'tbc.jpg',
+        options: {
+            option1: 'Auntie',
+
+            option2: 'Friend',
+
+            option3: 'Apple'
+        },
+        answer: "option2"
+    }
 ];
 let currentQuestion = 0;
 
@@ -76,13 +188,13 @@ function generateQuestions() {
         document.getElementById('question-number').innerHTML = `<h2> ${q.game_1}</h2>`;
         document.getElementById('question').innerHTML = `<h3> ${q.question} </h3>`;
 
-        
+
         var img = document.createElement("img");
-        img.src = "assets/images/hello.jpg";    
-        var src = document.getElementById("image");    
+        img.src = "assets/images/hello.jpg";
+        var src = document.getElementById("image");
         src.appendChild(img);
 
-               
+
 
         document.getElementById('option1').innerHTML = `<h2> ${q.options[0].option1}</h2>`;
         document.getElementById('option2').innerHTML = `<h2> ${q.options[1].option2}</h2>`;
@@ -92,9 +204,10 @@ function generateQuestions() {
 }
 
 
-    
+
+
 function checkAnswer() {
-     
+
 }
 
 
